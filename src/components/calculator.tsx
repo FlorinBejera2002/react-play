6e959eb5da6ed7a838eaaaa648b908a884a98367
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-10-27 16:12:10 +0300

    initial commit

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function calculator() {
+  const [result, setResult] = useState(0);
+  const [numberOne, setNumberOne] = useState(0);
+  const [numberTwo, setNumberTwo] = useState(0);
+  const calculateSum = () => setResult(numberOne + numberTwo);
+  const calculateDecrease = () => setResult(numberOne - numberTwo);
+  const calculateDivided = () => setResult(numberOne / numberTwo);
+  const calculateMultiply = () => setResult(numberOne * numberTwo);
+
   return (
     <div>
-      
+      <div className="container">
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <h1>Play-Calculator</h1>
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div
+              style={{ gap: "10px", display: "flex", flexDirection: "column" }}
+            >
+              <input
+                onChange={(e) => {
+                  setNumberOne(+e.target.value);
+                }}
+                type="text"
+                value={numberOne}
+              />
+              <button onClick={calculateSum}>+</button>
+              <button onClick={calculateDecrease}>-</button>
+            </div>
+
+            <div
+              style={{ gap: "10px", display: "flex", flexDirection: "column" }}
+            >
+              <input
+                type="text"
+                value={numberTwo}
+                onChange={(e) => {
+                  setNumberTwo(+e.target.value);
+                }}
+              />
+              <button onClick={calculateDivided}>/</button>
+              <button onClick={calculateMultiply}>x</button>
+            </div>
+          </div>
+
+          <h2>result : {result}</h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
