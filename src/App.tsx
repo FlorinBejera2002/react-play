4c74152c2cdaf66598d8e584a2d987c64e20e140
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-10-27 15:53:00 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,63 @@
 import "./App.css";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export const App = () => {
   const [result, setResult] = useState(0);
   const [numberOne, setNumberOne] = useState(0);
   const [numberTwo, setNumberTwo] = useState(0);
-  const calculateSum = () => {
-    const sum = numberOne + numberTwo;
-    setResult(sum);
-  };
-
-  const decreaseSum = () => {
-    const sum = numberOne - numberTwo;
-    setResult(sum);
-  };
-
-  const dividedSum = () => {
-    const sum = numberOne / numberTwo;
-    setResult(sum);
-  };
-
-  const multiplySum = () => {
-    const sum = numberOne * numberTwo;
-    setResult(sum);
-  };
+  const calculateSum = () => setResult(numberOne + numberTwo);
+  const calculateDecrease = () => setResult(numberOne - numberTwo);
+  const calculateDivided = () => setResult(numberOne / numberTwo);
+  const calculateMultiply = () => setResult(numberOne * numberTwo);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-      <h1>Play-Calculator</h1>
-      <div style={{ display: "flex", gap: "20px" }}>
-        <div style={{ gap: "10px", display: "flex", flexDirection: "column" }}>
-          <input
-            onChange={(e) => {
-              setNumberOne(+e.target.value);
-            }}
-            type="text"
-            value={numberOne}
-          />
-          <button onClick={calculateSum}>+</button>
-          <button onClick={decreaseSum}>-</button>
+    <div>
+      <div className="navbar">
+        <div>
+          <Link to="/">Home </Link>
         </div>
-
-        <div style={{ gap: "10px", display: "flex", flexDirection: "column" }}>
-          <input
-            type="text"
-            value={numberTwo}
-            onChange={(e) => {
-              setNumberTwo(+e.target.value);
-            }}
-          />
-          <button onClick={dividedSum}>/</button>
-          <button onClick={multiplySum}>x</button>
+        <div>
+          <Link to="/calculator"> Calculator </Link>
         </div>
+        <div><Link to="/aboutus"> About us </Link></div>
       </div>
+      {/* <div className="container">
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <h1>Play-Calculator</h1>
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div
+              style={{ gap: "10px", display: "flex", flexDirection: "column" }}
+            >
+              <input
+                onChange={(e) => {
+                  setNumberOne(+e.target.value);
+                }}
+                type="text"
+                value={numberOne}
+              />
+              <button onClick={calculateSum}>+</button>
+              <button onClick={calculateDecrease}>-</button>
+            </div>
 
-      <h2>result : {result}</h2>
+            <div
+              style={{ gap: "10px", display: "flex", flexDirection: "column" }}
+            >
+              <input
+                type="text"
+                value={numberTwo}
+                onChange={(e) => {
+                  setNumberTwo(+e.target.value);
+                }}
+              />
+              <button onClick={calculateDivided}>/</button>
+              <button onClick={calculateMultiply}>x</button>
+            </div>
+          </div>
+
+          <h2>result : {result}</h2>
+        </div> */}
+      {/* </div> */}
     </div>
   );
 };
