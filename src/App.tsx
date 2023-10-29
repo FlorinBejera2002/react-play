3dbe1409df9b4e6a08c22942e14ddf75937f52f0
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-10-29 10:03:48 +0200

    react-route-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./App.css";
 import Calculator from "./components/calculator"; 
-import About from "./components/about/about"; 
-import NotFound from "./components/NotFound";
+import About from "./components/about"; 
 import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Defineți componenta Home */}
+        <Route path="/" element={<App/>} /> 
         <Route path="/about" element={<About />} />
         <Route path="/calculator" element={<Calculator />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
